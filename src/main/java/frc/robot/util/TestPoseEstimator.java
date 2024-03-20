8a3dfed03884f8c87214d0af07eb60e75dd65b21
quinnhaygood <62080573+quinnhaygood@users.noreply.github.com>
2024-03-20 14:12:12 -0400

    Create TestPoseEstimator.java

---
+++ src/main/java/frc/robot/util/TestPoseEstimator.java
@@ -0,0 +1,289 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.util;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import edu.wpi.first.math.MathSharedStore;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.PoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Twist2d;
+import edu.wpi.first.math.interpolation.TimeInterpolatableBuffer;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveWheelPositions;
+import edu.wpi.first.math.kinematics.WheelPositions;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+
+/** Add your docs here. */
+public class TestPoseEstimator {
+
+  private static final double BUFFER_DURATION = 1.5;
+  private final TimeInterpolatableBuffer<Pose2d> poseBuffer = TimeInterpolatableBuffer.createBuffer(BUFFER_DURATION);
+
+  private final SwerveDriveKinematics kinematics;
+  private final Matrix<N3, N1> q = new Matrix<>(Nat.N3(), Nat.N1());
+  private final Matrix<N3, N3> visionK = new Matrix<>(Nat.N3(), Nat.N3());
+
+  private Pose2d odometryPose = new Pose2d();
+  private Pose2d estimatedPose = new Pose2d();
+
+  private SwerveDriveWheelPositions lastWheelPositions;
+  private Rotation2d lastGyroAngle;
+
+  /**
+   * Constructs a PoseEstimator.
+   *
+   * @param kinematics               A correctly-configured kinematics object for
+   *                                 your drivetrain.
+   * 
+   * @param stateStdDevs             Standard deviations of the pose estimate (x
+   *                                 position in meters, y position in meters, and
+   *                                 heading in radians). Increase these numbers
+   *                                 to trust your state estimate less.
+   * 
+   * @param visionMeasurementStdDevs Standard deviations of the vision pose
+   *                                 measurement (x position in meters, y position
+   *                                 in meters, and heading in radians). Increase
+   *                                 these numbers to trust the vision pose
+   *                                 measurement less.
+   */
+  public TestPoseEstimator(
+      SwerveDriveKinematics kinematics,
+      Matrix<N3, N1> stateStdDevs,
+      Matrix<N3, N1> visionStdDevs) {
+    this.kinematics = kinematics;
+
+    for (int i = 0; i < 3; ++i) {
+      q.set(i, 0, stateStdDevs.get(i, 0) * stateStdDevs.get(i, 0));
+    }
+    setVisionMeasurementStdDevs(visionStdDevs);
+  }
+
+  /**
+   * Sets the pose estimator's trust of global measurements. This might be used to
+   * change trust in vision measurements after the autonomous period, or to change
+   * trust as distance to a vision target increases.
+   *
+   * @param visionMeasurementStdDevs Standard deviations of the vision
+   *                                 measurements. Increase these numbers to trust
+   *                                 global measurements from vision less. This
+   *                                 matrix is in the form [x, y, theta]ᵀ, with
+   *                                 units in meters and radians.
+   */
+  public final void setVisionMeasurementStdDevs(Matrix<N3, N1> visionMeasurementStdDevs) {
+    var r = new double[3];
+    for (int i = 0; i < 3; ++i) {
+      r[i] = visionMeasurementStdDevs.get(i, 0) * visionMeasurementStdDevs.get(i, 0);
+    }
+
+    // Solve for closed form Kalman gain for continuous Kalman filter with A = 0
+    // and C = I. See wpimath/algorithms.md.
+    for (int row = 0; row < 3; ++row) {
+      if (q.get(row, 0) == 0.0) {
+        visionK.set(row, row, 0.0);
+      } else {
+        visionK.set(
+            row, row, q.get(row, 0) / (q.get(row, 0) + Math.sqrt(q.get(row, 0) * r[row])));
+      }
+    }
+  }
+
+  /**
+   * Resets the robot's position on the field.
+   *
+   * @param initialPose The position on the field that your robot is at.
+   */
+  public void resetPosition(Pose2d initialPose, SwerveDriveWheelPositions wheelPositions) {
+    odometryPose = initialPose;
+    estimatedPose = initialPose;
+    this.lastWheelPositions = wheelPositions;
+
+    poseBuffer.clear();
+  }
+
+  /**
+   * Gets the odometry robot pose.
+   *
+   * @return The odometry robot pose in meters.
+   */
+  public Pose2d getOdometryPosition() {
+    return odometryPose;
+  }
+
+  /**
+   * Gets the estimated robot pose.
+   *
+   * @return The estimated robot pose in meters.
+   */
+  public Pose2d getEstimatedPosition() {
+    return estimatedPose;
+  }
+
+  /**
+   * Updates the pose estimator with wheel encoder and gyro information. This
+   * should be called every loop.
+   *
+   * @param gyroAngle      The current gyro angle.
+   * @param wheelPositions The current encoder readings.
+   */
+  public void update(Optional<Rotation2d> gyroAngle, SwerveDriveWheelPositions wheelPositions) {
+    updateWithTime(MathSharedStore.getTimestamp(), gyroAngle, wheelPositions);
+  }
+
+  /**
+   * Updates the pose estimator with wheel encoder and gyro information. This
+   * should be called every loop.
+   *
+   * @param currentTimeSeconds Time at which this method was called, in seconds.
+   * @param gyroAngle          The current gyro angle.
+   * @param wheelPositions     The current encoder readings.
+   */
+  public void updateWithTime(
+      double currentTimeSeconds,
+      Optional<Rotation2d> gyroAngle,
+      SwerveDriveWheelPositions wheelPositions) {
+
+    Twist2d twist = kinematics.toTwist2d(lastWheelPositions, wheelPositions);
+
+    if (gyroAngle.isPresent()) {
+      twist = new Twist2d(
+          twist.dx,
+          twist.dy,
+          gyroAngle.get().minus(lastGyroAngle).getRadians());
+      lastGyroAngle = gyroAngle.get();
+    }
+
+    odometryPose = odometryPose.exp(twist);
+
+    poseBuffer.addSample(
+        currentTimeSeconds,
+        getOdometryPosition());
+
+    estimatedPose.exp(twist);
+  }
+
+  /**
+   * Adds a vision measurement to the Kalman Filter. This will correct the
+   * odometry pose estimate while still accounting for measurement noise.
+   *
+   * <p>
+   * This method can be called as infrequently as you want, as long as you are
+   * calling {@link PoseEstimator#update} every loop.
+   *
+   * <p>
+   * To promote stability of the pose estimate and make it robust to bad vision
+   * data, we recommend only adding vision measurements that are already within
+   * one meter or so of the current pose estimate.
+   *
+   * @param visionRobotPoseMeters The pose of the robot as measured by the vision
+   *                              camera.
+   * @param timestampSeconds      The timestamp of the vision measurement in
+   *                              seconds. Note that if you don't use your own
+   *                              time source by calling {@link
+   *                              PoseEstimator#updateWithTime(double,Rotation2d,WheelPositions)}
+   *                              then you must use a timestamp with an epoch
+   *                              since FPGA startup (i.e., the epoch of this
+   *                              timestamp is the same epoch as
+   *                              {@link edu.wpi.first.wpilibj.Timer#getFPGATimestamp()}.)
+   *                              This means that you should use
+   *                              {@link edu.wpi.first.wpilibj.Timer#getFPGATimestamp()}
+   *                              as your time source or sync the epochs.
+   */
+  public void addVisionMeasurement(Pose2d visionRobotPoseMeters, double timestampSeconds) {
+    // Step 0: If this measurement is old enough to be outside the pose buffer's
+    // timespan, skip.
+    try {
+      if (poseBuffer.getInternalBuffer().lastKey() - BUFFER_DURATION > timestampSeconds) {
+        return;
+      }
+    } catch (NoSuchElementException ex) {
+      return;
+    }
+
+    // Step 1: Get the pose odometry measured at the moment the vision measurement
+    // was made.
+    var sample = poseBuffer.getSample(timestampSeconds);
+
+    if (sample.isEmpty()) {
+      return;
+    }
+
+    // Credit to 6328
+    // sample --> odometryPose transform and backwards of that
+    var sampleToOdometryTransform = new Transform2d(sample.get(), odometryPose);
+    var odometryToSampleTransform = new Transform2d(odometryPose, sample.get());
+    // get old estimate by applying odometryToSample Transform
+    Pose2d estimateAtTime = estimatedPose.plus(odometryToSampleTransform);
+
+    // difference between estimate and vision pose
+    Transform2d transform = new Transform2d(estimateAtTime, visionRobotPoseMeters);
+    // scale transform by visionK
+    var kTimesTransform = visionK.times(
+        VecBuilder.fill(
+            transform.getX(), transform.getY(), transform.getRotation().getRadians()));
+    // convert from Matrix to Transform2d
+    Transform2d scaledTransform = new Transform2d(
+        kTimesTransform.get(0, 0),
+        kTimesTransform.get(1, 0),
+        Rotation2d.fromRadians(kTimesTransform.get(2, 0)));
+
+    // Recalculate current estimate by applying scaled transform to old estimate
+    // then replaying odometry data
+    estimatedPose = estimateAtTime.plus(scaledTransform).plus(sampleToOdometryTransform);
+  }
+
+  /**
+   * Adds a vision measurement to the Kalman Filter. This will correct the
+   * odometry pose estimate while still accounting for measurement noise.
+   *
+   * <p>
+   * This method can be called as infrequently as you want, as long as you are
+   * calling {@link PoseEstimator#update} every loop.
+   *
+   * <p>
+   * To promote stability of the pose estimate and make it robust to bad vision
+   * data, we recommend only adding vision measurements that are already within
+   * one meter or so of the current pose estimate.
+   *
+   * <p>
+   * Note that the vision measurement standard deviations passed into this method
+   * will continue to apply to future measurements until a subsequent call to
+   * {@link PoseEstimator#setVisionMeasurementStdDevs(Matrix)} or this method.
+   *
+   * @param visionRobotPoseMeters    The pose of the robot as measured by the
+   *                                 vision camera.
+   * @param timestampSeconds         The timestamp of the vision measurement in
+   *                                 seconds. Note that if you don't use your own
+   *                                 time source by calling
+   *                                 {@link #updateWithTime}, then you must use a
+   *                                 timestamp with an epoch since FPGA startup
+   *                                 (i.e., the epoch of this timestamp is the
+   *                                 same epoch as
+   *                                 {@link edu.wpi.first.wpilibj.Timer#getFPGATimestamp()}).
+   *                                 This means that you should use
+   *                                 {@link edu.wpi.first.wpilibj.Timer#getFPGATimestamp()}
+   *                                 as your time source in this case.
+   * @param visionMeasurementStdDevs Standard deviations of the vision pose
+   *                                 measurement (x position in meters, y position
+   *                                 in meters, and heading in radians). Increase
+   *                                 these numbers to trust the vision pose
+   *                                 measurement less.
+   */
+  public void addVisionMeasurement(
+      Pose2d visionRobotPoseMeters,
+      double timestampSeconds,
+      Matrix<N3, N1> visionMeasurementStdDevs) {
+    setVisionMeasurementStdDevs(visionMeasurementStdDevs);
+    addVisionMeasurement(visionRobotPoseMeters, timestampSeconds);
+  }
+
+}
