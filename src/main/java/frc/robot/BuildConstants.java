cde016da9a02051b14f344560cb27926dbf6a0bd
quinnhaygood <62080573+quinnhaygood@users.noreply.github.com>
2024-03-20 17:37:24 -0400

    fixed odom issue

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "3655TractorTechnicians_2024_crescendo";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 166;
-  public static final String GIT_SHA = "ccc07067bce989021243323af344c218888077d0";
-  public static final String GIT_DATE = "2024-03-20 17:41:08 EDT";
+  public static final int GIT_REVISION = 170;
+  public static final String GIT_SHA = "2859aa23d82fbd5c5f310eca2f29d7efec78cbd2";
+  public static final String GIT_DATE = "2024-03-20 18:14:53 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-03-20 18:11:29 EDT";
-  public static final long BUILD_UNIX_TIME = 1710972689519L;
+  public static final String BUILD_DATE = "2024-03-20 17:32:49 EDT";
+  public static final long BUILD_UNIX_TIME = 1710970369915L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
