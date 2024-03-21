a0080fe49f8c338e470ccaf13f0cad117eee658c
driverStation <quinnhaygood@gmail.com>
2024-03-20 17:34:46 -0700

    auto tuning

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "3655TractorTechnicians_2024_crescendo";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 161;
-  public static final String GIT_SHA = "fae6dc963d6ba2b8763ef8642919aa1bf3097080";
-  public static final String GIT_DATE = "2024-03-19 20:30:40 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-03-20 11:19:34 EDT";
-  public static final long BUILD_UNIX_TIME = 1710947974533L;
+  public static final int GIT_REVISION = 162;
+  public static final String GIT_SHA = "bacb783522b5be004f83010f039c5dd9952007ff";
+  public static final String GIT_DATE = "2024-03-20 11:47:35 EDT";
+  public static final String GIT_BRANCH = "no_drive_testing";
+  public static final String BUILD_DATE = "2024-03-20 20:14:57 EDT";
+  public static final long BUILD_UNIX_TIME = 1710980097624L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
